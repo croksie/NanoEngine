bc834ae1a69d8c4de767bf73008ce35ab4a60f69
Guillaume <servantguillaume6@gmail.com>
2026-08-08 18:43:30 +0200

    feat(logging): add logging utility using spdlog

---
+++ src/utils/Log.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include <spdlog/spdlog.h>
+
+
+#define ENGINE_LOG_TRACE(...)  SPDLOG_TRACE(__VA_ARGS__)
+#define ENGINE_LOG_DEBUG(...) SPDLOG_DEBUG(__VA_ARGS__)
+#define ENGINE_LOG_INFO(...) SPDLOG_INFO(__VA_ARGS__)
+#define ENGINE_LOG_WARN(...) SPDLOG_WARN(__VA_ARGS__)
+#define ENGINE_LOG_ERROR(...) SPDLOG_ERROR(__VA_ARGS__)
+#define ENGINE_LOG_CRITICAL(...) SPDLOG_CRITICAL(__VA_ARGS__)
+
+
+
