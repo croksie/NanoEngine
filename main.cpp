be510aacf29d5c241aec7e5b2378cada1e036dda
Guillaume <servantguillaume6@gmail.com>
2026-08-20 23:21:21 +0200

    feat: add a fps counter in the main loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <core/window.h>
-#include <rendering/renderer.h>
-#include <utils/log.h>
+#include "core/window.h"
+#include "rendering/renderer.h"
+#include "utils/log.h"
 
 int main() 
 {
@@ -14,10 +14,25 @@ int main()
     Renderer renderer;
     renderer.initialize(&window);
 
+    static auto lastTime = std::chrono::high_resolution_clock::now();
+    static int frameCount = 0;
+
     while(!window.windowSouldClose()) {
         renderer.render();
-    }
 
+        // FPS Counter
+        frameCount++;
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        float elapsed = std::chrono::duration<float>(currentTime - lastTime).count();
+
+        if (elapsed >= 1.0f) {
+            ENGINE_LOG_INFO("FPS: {} ({} ms/frame)", frameCount ,1000.0f / frameCount);
+            frameCount = 0;
+            lastTime = currentTime;
+        }
+    }
+    ENGINE_LOG_INFO("NanoEngine shutting down ...");
+    renderer.shutdown();
 
     return 0;
     
