b4478e261aeb94561ea887e170bf53be244202e0
Guillaume <servantguillaume6@gmail.com>
2026-08-23 11:26:26 +0200

    feat:implement callback function for window resize

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -15,13 +15,25 @@ Window::Window() {
 void Window::initializeWindow(const int width, const int height, const char * title)
 {
     m_window = glfwCreateWindow(width, height, title, NULL, NULL);
+    glfwSetWindowUserPointer(m_window, this);
+
+    glfwSetWindowSizeCallback(m_window, [](GLFWwindow* window, int width, int height) {
+        auto* instance = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+        
+        if (instance && instance->m_onSizeChanged) {
+            instance->m_onSizeChanged(width, height);
+        }
+    });
+
     if(!m_window){
         glfwTerminate();
         ENGINE_LOG_CRITICAL("Error while creating window");
         throw std::runtime_error("Error while creating window");
     }
+    
     ENGINE_LOG_DEBUG("Window created");
 }
+
 
 Window::~Window() {
     glfwDestroyWindow(m_window);
--- src/core/window.h
+++ src/core/window.h
@@ -4,12 +4,15 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <functional>
 
 
+using SizeCallback = std::function<void(int width, int height)>;
 
 class Window {
 public:
     void initializeWindow(const int width, const int height, const char* title);
+    void setWindowSizeCallback(SizeCallback cb) { m_onSizeChanged = cb; }
 
     void* getNativeHandle() const { return static_cast<void*>(m_window); }
     bool windowSouldClose() const { return glfwWindowShouldClose(m_window); }
@@ -19,5 +22,5 @@ public:
 
 private:
     GLFWwindow* m_window;
-
+    SizeCallback m_onSizeChanged;
 };
