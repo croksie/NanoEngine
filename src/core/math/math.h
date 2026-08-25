b9ed7922021bdb7cb03d6dd058840cf0a914a0f5
Guillaume <servantguillaume6@gmail.com>
2026-08-25 23:32:54 +0200

    feat : implement transform struct

--- src/core/math/math.h
+++ src/core/math/math.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "core/math/vec.h"
+#include "core/math/transform.h"
---
+++ src/core/math/transform.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "core/math/vec.h"
+
+struct Transform {
+    Vec3 Position;
+    Vec3 Rotation;
+    Vec3 Scale;
+};
