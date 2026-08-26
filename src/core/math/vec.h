1004667fd627d4d50744621858ffbd507c21114c
Guillaume <servantguillaume6@gmail.com>
2026-08-26 18:41:31 +0200

    feat : implement missing funtion in vec.h

--- src/core/math/vec.h
+++ src/core/math/vec.h
@@ -8,6 +8,7 @@ struct Vec2
 
     Vec2() : x(0.0f), y(0.0f) {}
     Vec2(float x, float y) : x(x), y(y) {}
+    Vec2(const glm::vec2& v) : x(v.x), y(v.y) {}
 
     Vec2 operator+(const Vec2& other) const {
         return Vec2{x + other.x, y + other.y};
@@ -15,6 +16,18 @@ struct Vec2
 
     Vec2 operator-(const Vec2& other) const {
         return Vec2{x - other.x, y - other.y};
+    }
+
+    Vec2 operator-() const {
+        return Vec2{-x, -y};
+    }
+
+    Vec2 operator*(float scalar) const {
+        return Vec2{x * scalar, y * scalar};
+    }
+
+    Vec2 operator/(float scalar) const {
+        return Vec2{x / scalar, y / scalar};
     }
 
     Vec2& operator+=(const Vec2& other) {
@@ -42,6 +55,7 @@ struct Vec3
 
     Vec3() : x(0.0f), y(0.0f), z(0.0f) {}
     Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+    Vec3(const glm::vec3& v) : x(v.x), y(v.y), z(v.z) {}
 
     Vec3 operator+(const Vec3& other) const {
         return Vec3{x + other.x, y + other.y, z + other.z};
@@ -49,6 +63,18 @@ struct Vec3
 
     Vec3 operator-(const Vec3& other) const {
         return Vec3{x - other.x, y - other.y, z - other.z};
+    }
+
+    Vec3 operator-() const {
+        return Vec3{-x, -y, -z};
+    }
+
+    Vec3 operator*(float scalar) const {
+        return Vec3{x * scalar, y * scalar, z * scalar};
+    }
+
+    Vec3 operator/(float scalar) const {
+        return Vec3{x / scalar, y / scalar, z / scalar};
     }
 
     Vec3& operator+=(const Vec3& other) {
@@ -79,6 +105,7 @@ struct Vec4
 
     Vec4() : x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
     Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+    Vec4(const glm::vec4& v) : x(v.x), y(v.y), z(v.z), w(v.w) {}
 
     Vec4 operator+(const Vec4& other) const {
         return Vec4{x + other.x, y + other.y, z + other.z, w + other.w};
@@ -86,6 +113,18 @@ struct Vec4
 
     Vec4 operator-(const Vec4& other) const {
         return Vec4{x - other.x, y - other.y, z - other.z, w - other.w};
+    }
+
+    Vec4 operator-() const {
+        return Vec4{-x, -y, -z, -w};
+    }
+
+    Vec4 operator*(float scalar) const {
+        return Vec4{x * scalar, y * scalar, z * scalar, w * scalar};
+    }
+
+    Vec4 operator/(float scalar) const {
+        return Vec4{x / scalar, y / scalar, z / scalar, w / scalar};
     }
 
     Vec4& operator+=(const Vec4& other) {
